e2df63087ae4a88bf1dc4001cf6b477105baeec4
THONWELLING <thonwellingdani13@gmail.com>
2023-05-09 20:10:08 -0300

    Separando o Cliente http

--- src/App.java
+++ src/App.java
@@ -13,35 +13,34 @@ public class App {
 
     //fazer um conexão http e buscar os top 250 filmes
     String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-    URI endereco = URI.create(url);
-    HttpClient client = HttpClient.newHttpClient();
-    HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    String body = response.body();
 
-    //Pegar os mais populares
+
+    var http = new ClienteHttp();
+    String json = http.buscaDados(url);
+
+   /* //Pegar os mais populares
     String urlMostPopular = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
     URI enderecoMostPopular = URI.create(urlMostPopular);
     HttpClient client1 = HttpClient.newHttpClient();
     HttpRequest requisicao = HttpRequest.newBuilder(enderecoMostPopular).GET().build();
     HttpResponse<String> resposta = client1.send(requisicao, HttpResponse.BodyHandlers.ofString());
     String corpo = resposta.body();
-
+  */
 
     //extrair só os dados que interessam (titulo,  poster, classificação)
     var parser = new JsonParser();
-    List<Map<String, String>> listaDeFilmes = parser.parse(body);
+    List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
-    List<Map<String, String>> maisPopulares = parser.parse(corpo);
+    List<Map<String, String>> maisPopulares = parser.parse(json);
 
 
     //exibir e manipular os dados
       var gerador = new GeradorDeFigurinhas();
     for ( int i = 0; i < 10; i++) {
-      Map<String, String> filme = listaDeFilmes.get(i);
+      Map<String, String> conteudo = listaDeConteudos.get(i);
 
-      String urlImagem = filme.get("image").replace("((@+)(.*).jpg$", "$1.jpg");
-      String titulo = filme.get("title");
+      String urlImagem = conteudo.get("image").replace("((@+)(.*).jpg$", "$1.jpg");
+      String titulo = conteudo.get("title");
 
       InputStream inputStream = new URL(urlImagem).openStream();
       String nomeArquivo = "saida/" + titulo + ".png";
---
+++ src/ClienteHttp.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClienteHttp {
+  public String buscaDados (String url) {
+    try{
+      URI endereco = URI.create(url);
+      HttpClient client = HttpClient.newHttpClient();
+      HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      return response.body();
+
+    } catch(IOException | InterruptedException exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+}
